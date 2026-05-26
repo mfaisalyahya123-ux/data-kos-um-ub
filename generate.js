918f1a189497030b2c1156954fd0c02d38c30cfa
mfaisalyahya123-ux <mfaisalyahya123@gmail.com>
2026-05-26 22:01:52 +0700

    Change: Title from 'Data Kos UM/UB' to 'Keuangan Kos UM/UB v1.2'

--- generate.js
+++ generate.js
@@ -20,7 +20,7 @@ const html = `<!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Data Kos UM/UB - Keuangan v1.2</title>
+  <title>Keuangan Kos UM/UB v1.2</title>
   <style>
     * {
       margin: 0;
